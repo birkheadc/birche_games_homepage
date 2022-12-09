476d7fc0d022357405c62e0b7a292808a7b73645
Colby Birkhead <birkheadc@gmail.com>
2022-12-10 01:03:32 +0900

    Refactor player size calculations

--- src/components/gamePlayer/GamePlayer.tsx
+++ src/components/gamePlayer/GamePlayer.tsx
@@ -12,39 +12,20 @@ function GamePlayer(props: IGamePlayerProps): JSX.Element {
   const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
+    
     function calculatePlayerSize(): { width: number, height: number } {
-      const defaultSize = {
-        width: 600 * props.game.aspectRatio,
-        height: 600
+      const wrapperWidth = document.querySelector('#player-main-wrapper')?.clientWidth || 600;
+      const width = wrapperWidth * 0.8;
+      return {
+        width: width,
+        height: width / props.game.aspectRatio
       }
-
-      const wrapper = document.querySelector('#player-main-wrapper');
-      if (wrapper == null) {
-        return defaultSize;
-      }
-
-      const windowWidth = wrapper.clientWidth;
-      const windowHeight = wrapper.clientHeight;
-
-      console.log('width: ', windowWidth, 'height: ', windowHeight);
-
-      if (windowWidth * props.game.aspectRatio > windowHeight) {
-        return {
-          width: windowHeight * props.game.aspectRatio,
-          height: windowHeight
-        }
-      }
-      else {
-        return {
-          width: windowWidth,
-          height: windowWidth / props.game.aspectRatio
-        }
-      }
-      
     }
+    
     const playerSize = calculatePlayerSize();
     document.documentElement.style.setProperty('--player-width', playerSize.width.toString() + 'px');
     document.documentElement.style.setProperty('--player-height', playerSize.height.toString() + 'px');
+
     api.loadGameScript('dejavu', () => {
       setLoading(false);
     });
