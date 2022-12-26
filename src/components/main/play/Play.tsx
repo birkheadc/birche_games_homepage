628a02f977cc1fd2720d971d198d5c00b1e4f218
Colby Birkhead <birkheadc@gmail.com>
2022-12-26 20:15:05 +0900

    add description

--- src/components/main/play/Play.tsx
+++ src/components/main/play/Play.tsx
@@ -20,6 +20,11 @@ function Play(props: IPlayProps): JSX.Element {
       {/* TODO: Make title dynamic */}
       <h1>DEJAVU</h1>
       <GamePlayer game={game}/>
+      <p className='player-description'>Dejavu was created in 1 month for Game Devcember 2022. It is the first game I have published.
+        Some sections turned out much more difficult than I intended, and the story evolved into something much darker than I anticipated.
+        All the same, it was fun to create, and I hope you had fun playing it is well.
+        Thank you for playing.
+      </p>
     </div>
   );
 }
