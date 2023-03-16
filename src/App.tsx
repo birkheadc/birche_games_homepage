329712448e96f4415c7f75176045e1b5b96b2af3
Colby Birkhead <birkheadc@gmail.com>
2023-03-16 12:22:20 +0900

    remove unnecessary background component; fix issue with browser card descriptions messing with screen size when not visible

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,8 @@ import './fonts.css';
 import './vars.css';
 
 import './App.css'
-import Background from './components/background/Background';
 import Main from './components/main/Main';
 import Nav from './components/nav/Nav';
-import LoadingPage from './components/loadingPage/LoadingPage';
-import api from './api';
-import { IGameProfile } from './api/fetchGameProfiles';
 import Footer from './components/footer/Footer';
 
 interface IAppProps {
@@ -22,7 +18,6 @@ function App(props: IAppProps): JSX.Element {
 
   return (
     <BrowserRouter>
-      <Background />
       <Nav />
       <Main />
       <Footer />
